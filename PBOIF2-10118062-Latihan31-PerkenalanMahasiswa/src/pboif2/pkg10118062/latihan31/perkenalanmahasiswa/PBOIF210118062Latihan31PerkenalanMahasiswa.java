7c27950f9be059ae40797150b93ad6e889f2d7d3
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-10-17 21:55:13 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan31-PerkenalanMahasiswa/src/pboif2/pkg10118062/latihan31/perkenalanmahasiswa/PBOIF210118062Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author  ASUS
+ */
+public class PBOIF210118062Latihan31PerkenalanMahasiswa {
+
+    public String nama;
+    public String nim;
+        
+    public void perkenalkanDiri() {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM " + nim);
+        System.out.println("My Name is " + nama);
+        System.out.println("");
+        
+       
+    
+        
+    }
+    
+}
---
+++ PBOIF2-10118062-Latihan31-PerkenalanMahasiswa/src/pboif2/pkg10118062/latihan31/perkenalanmahasiswa/mainclass.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author  ASUS
+ */
+public class mainclass {
+    public static void main(String[] args) {
+        PBOIF210118062Latihan31PerkenalanMahasiswa mhs = new PBOIF210118062Latihan31PerkenalanMahasiswa();
+        
+        mhs.nim = "10118062";
+        mhs.nama = "M Dandri Handian";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110270";
+        mhs.nama = "Indra Tiola";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110271";
+        mhs.nama = "Robi Tanzil Ganafi";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110269";
+        mhs.nama = "Muhammad Nur Awaludin";
+        
+        mhs.perkenalkanDiri();
+        
+
+    }
+    
+}
